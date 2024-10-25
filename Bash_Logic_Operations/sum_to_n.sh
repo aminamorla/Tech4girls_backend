d188e8c5cd5c1a47f0479302ea371cb0cf435a05
aminamorla <rabiuamina14@gmail.com>
2024-10-25 08:25:55 +0000

    task8

--- Bash_Logic_Operations/sum_to_n.sh
+++ Bash_Logic_Operations/sum_to_n.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#n=$1
-#sum=$
-#if ! [[ $n =~ ^[0-9]+$ ]]; then
-n=4
-
-if [ $sum -ne 1 ]; then
-echo "The sum of numbers from 1 to 5 is: $sum"
-fi
+echo "Enter number"
+read n
+sum=o
+for (( i=1; i<=n; i++ ))
+do
+sum=$((sum+i))
+done
+  echo "The sum of numbers from 1 to $n is: $sum"
