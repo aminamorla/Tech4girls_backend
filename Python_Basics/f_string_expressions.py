5915cdeb2395f8980e436f5a3da93f1173d2cc62
aminamorla <rabiuamina14@gmail.com>
2024-10-26 21:46:38 +0000

    task4

---
+++ Python_Basics/f_string_expressions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#  f_string_expressions.py
+#  Define variables
+a = 10
+b = 20
+
+sum = a + b
+diff = a - b
+prod = a * b
+print(f"The sum of {a} and {b} is {sum}.")
+print(f"The difference of {a} and {b} is {diff}.")
+print(f"The product of {a} and {b} is {prod}.")
