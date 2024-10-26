39e09a4b207119a5c2ea3c4962d8f464cdd1adc0
aminamorla <rabiuamina14@gmail.com>
2024-10-26 19:21:37 +0000

    task3

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+message = "Hello, Baby"
+print(len(message))
+print(message.upper())
+print(message.lower())
+print(message.replace('Baby', 'Hunny'))
+print(message.join(['Hello', 'Baby']))
