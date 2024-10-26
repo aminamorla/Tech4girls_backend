b9199f701c7c2d8c2c4996ca4533caf7d6fd2df8
aminamorla <rabiuamina14@gmail.com>
2024-10-26 22:11:52 +0000

    task5

---
+++ Python_Basics/string_indexing.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+sentence = "Python is powerful and easy to learn."
+
+print(len(sentence))
+
+print(f"The first character is:{sentence[0]}")
+print(f"The last character is: {sentence[-1]}")
+print(f"The word 'powerful' is: {sentence[10:18]}")
