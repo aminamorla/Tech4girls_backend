c2991c40339bc070cd0e56b71fbefdb0813556a4
aminamorla <rabiuamina14@gmail.com>
2024-10-27 20:45:54 +0000

    task2

---
+++ Python_Control_Flow/list_and_set_methods.py
@@ -0,0 +1,52 @@
+     #LIST METHODS
+#append() is used to add elements to the end of a list.
+#my_list = [1, 2, 3]
+#my_list.append(6)
+#print(my_list)
+
+#remove() is used to remove items from a list.
+#my_list = [1, 2, 3]
+#my_list.remove(2)
+#print(my_list)
+
+#pop() it removes the last item inside a list and returns it.
+#popped_item = my_list.pop()
+#print("popped item:", popped_item)
+
+#sort() is used to sort numbers or change numbers from ascending to descending order.
+#my_list.sort()
+#print(my_list.sort())
+
+#reverse() it reverse the list. The last item becomes first and the first becomes last.
+#my_list.reverse
+#print(my_list)
+
+#extend() add element from another interable to the end of the list.
+#my_list.extend([4, 5])
+#print(my_list)
+
+      #SET METHODS
+#set_whole = {0, 1, 2, 3}
+#set_even = {2, 4, 6, 8}
+
+#add() adds an element to the set.
+# set_whole.add(4)
+# print(set_whole)
+ 
+#remove() removes last or specified element from a set.
+# set_whole.remove(4)
+# print(set_whole)
+
+# union() returns a set that is the union of two sets.
+# union_set = set_whole.union(set_even)
+# print("union of set_whole and set_even")
+ 
+# intersection() it print out common values in two sets.
+# intersection_set = set_whole.intersection(set_even)
+# print(set_whole.intersection(set_even)) 
+ 
+# difference() returns a set that contains elements in set_whole but not in set_even.
+# difference_set = set_whole.difference(set_even)
+# print(set_whole.difference(set_even))  
+
+
