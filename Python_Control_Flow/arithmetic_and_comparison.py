836c3478235345ae4c147c1e7c3948dcc16e735e
aminamorla <rabiuamina14@gmail.com>
2024-10-30 08:59:32 +0000

    task6

---
+++ Python_Control_Flow/arithmetic_and_comparison.py
@@ -0,0 +1,29 @@
+x = 5
+y = 8
+sum = x + y
+print(f"sum (x + y): {sum}")
+
+difference = x - y
+print(f"difference(x - y): {difference}")
+
+product = x * y
+print(f"product(x * y): {product}")
+
+division = x / y
+print(f"division(x / y): {division}")
+
+int_division = x // y
+print(f"int_division(x // y): {int_division}")
+
+exponent = x ** y
+print(f"exponent(x ** y): {exponent}")
+
+modulu = x % y
+print(f'modulu(x % y): {modulu}')
+
+print(x == y)
+print(x != y)
+print(x < y)
+print(x > y)
+print(x <= y)
+print(x >= y)
